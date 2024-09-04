732b8e4065a7bf10f34e1d5b261f6255f41164cf
vodjanoj <vladimirs.vodjanojs@gmail.com>
2024-09-04 20:17:41 +0300

    finished project

---
+++ src/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Welcome to EUcasinolist.com. Here we offer you carefully vetted and tested casinos that, with Trustly and Instank Bank, provide the best casino experience in Europe. You can rest and be assured of only high-quality casinos in our list.">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+    <title>Top EU Casinos 2024!</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="header__inner">
+          <div class="header__logo">
+            <a class="header__logo-link" href="#">
+              <img
+                class="header__logo-img"
+                src="images/top-casino-logo.svg"
+                alt="Top Casinos EU"
+              />
+            </a>
+          </div>
+          <div class="header__present">
+            <div class="header__present_icon">
+              <a class="header__present-link" href="#">
+                <img
+                  class="header__present_icon-img"
+                  src="images/present.png"
+                  alt="Top Casinos Present"
+                />
+              </a>
+              <span class="header__present_count">2</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main class="casinos">
+      <section class="casinos__details">
+        <div class="container">
+          <h1 class="casinos__title">Top EU Casinos 2024</h1>
+          <p class="casinos__description">
+            Welcome to TOPcasinoseu.com. We present a selection of meticulously
+            vetted and tested casinos that utilize Trustly and Instant Bank,
+            ensuring the best casino experience in Europe. You can relax,
+            knowing that our list includes only high-quality, reliable casinos.
+          </p>
+        </div>
+      </section>
+      <section class="casinos__offers">
+        <div class="container">
+          <div class="casinos__inner">
+            <div class="casino__offer">Title of Casino</div>
+            <div class="casino__play-details">Play details</div>
+            <div class="casino__payment-methods">Payment methods</div>
+            <div class="casino-description">Description</div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
